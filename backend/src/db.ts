42affcf8d95308bd024957a50309b48cee46a8fa
Nishul Dhakar <nishuldhakar.se@gmail.com>
2025-06-25 17:01:06 +0530

    changed BE url

--- backend/src/db.ts
+++ backend/src/db.ts
@@ -1,5 +1,6 @@
 import mongoose, { Schema, model } from "mongoose";
 import "dotenv/config";
+import { number } from "zod";
 
 if (!process.env.MONGO_URL) {
   throw new Error("MONGO_URL env variable is missing");
@@ -15,7 +16,26 @@ mongoose.connect(process.env.MONGO_URL)
 
 const userSchema = new Schema({
   username: { type: String, required: true, unique: true },
-  password: { type: String, required: true }
+  password: { type: String, required: true },
+  firstname:{ type: String, required: true },
+  lastname:{ type: String, required: true }
+});
+
+
+const accountSchema = new Schema({
+
+  userId : { 
+    type : mongoose.Schema.Types.ObjectId,
+    ref : 'UserModel',
+   require : true
+  },
+  balance : {
+    type : Number,
+    require : true
+  }
 });
 
 export const UserModel = model("User", userSchema);
+export const AccountModel = model("Account" , accountSchema)
+
+
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,14 +1,15 @@
-import express, { Request, Response } from 'express';
+import express from 'express';
+import mainRouter from './router/index';
+import cors from 'cors';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello, World!');
-});
+app.use('/api/v1', mainRouter); 
 
-app.listen(3001, () => {
-  console.log(`Server is running on http://localhost:3001`);
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
-
